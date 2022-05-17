6fd917244728bedcaa65078844f6a46bf4ec90da
João Ricardo Lemes Polachin <joao.polachin@programmers.com.br>
2022-05-17 10:24:25 -0300

    correto 8

--- Exercicio7/Program.cs
+++ Exercicio7/Program.cs
@@ -3,14 +3,14 @@ int v = 4;
 
 for (v = 0; v < 4; v++)
 {
-    Console.WriteLine($"Digite a nota {v + 1}");
+    Console.Write($"Digite a nota {v + 1}: ");
     N[v] = double.Parse(Console.ReadLine());
 
 
 }
 
 double media = (N[0] + N[1] + N[2] + N[3]) / v;
-Console.WriteLine(media);
+Console.WriteLine($"A média das notas digitadas é: {media}");
 
 if (media < 7)
 {
